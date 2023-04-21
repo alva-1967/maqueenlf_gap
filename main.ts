b22ed663078ece35a3bd84db15d5359bae96d995
alva-1967 <131169876+alva-1967@users.noreply.github.com>
2023-04-21 11:55:33 +0100

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,20 @@
+basic.showIcon(IconNames.No)
+basic.pause(1000)
+basic.clearScreen()
+led.enable(false)
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
+    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+        maqueen.motorStop(maqueen.Motors.All)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+        basic.pause(100)
+    }
+})
